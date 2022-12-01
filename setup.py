e2b5dd4db59fbea1ecae3080da8e149a5243ceb6
sharvanisireesha <sharvanisireesha@gmail.com>
2022-12-01 22:54:39 +0530

    Updated  files

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import find_packages,setup
+from typing import list
 
-def get_requirements():
-    pass
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPEN_E_DOT = "-e."
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirementlist = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+
+        if HYPEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPEN_E_DOT)
+            return requirement_list
 
 
-
-setup{
-    name:"sensor"
-    version : "0.0.1"
-    author:"sireesha"
-    author_email :"sharvanisireesha@gmail.com"
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="sireesha",
+    author_email ="sharvanisireesha@gmail.com",
     packages = find_packages(),
     install_requires = get_requirements()
-}
+)
